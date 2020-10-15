51fe2bea8da657958aade154e229c3bdf0013801
Joy Evans <joyevans@me.com>
2020-10-15 17:43:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def greet_characters(array)
   end
   
 end
-greet_characters (dwarves)
+greet_characters (array)
   
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
